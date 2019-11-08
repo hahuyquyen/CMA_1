cdf55aa772adcad1648a0ee977324c5cb0f0b37c
Riki <annhandt09@gmail.com>
2019-11-08 21:50:32 +0700

    Create SD_Test.ino

---
+++ SD_Test/SD_Test.ino
@@ -0,0 +1,99 @@
+/*
+ * Connect the SD card to the following pins:
+ *
+ * SD Card | ESP32
+ *    D2       -
+ *    D3       SS
+ *    CMD      MOSI
+ *    VSS      GND
+ *    VDD      3.3V
+ *    CLK      SCK
+ *    VSS      GND
+ *    D0       MISO
+ *    D1       -
+ */
+#include "FS.h"
+#include "SD.h"
+#include "SPI.h"
+#define SD_CS 5
+void listDir(fs::FS &fs, const char * dirname, uint8_t levels){
+    Serial.printf("Listing directory: %s\n", dirname);
+
+    File root = fs.open(dirname);
+    if(!root){
+        Serial.println("Failed to open directory");
+        return;
+    }
+    if(!root.isDirectory()){
+        Serial.println("Not a directory");
+        return;
+    }
+
+    File file = root.openNextFile();
+    while(file){
+        if(file.isDirectory()){
+            Serial.print("  DIR : ");
+            Serial.println(file.name());
+            if(levels){
+                listDir(fs, file.name(), levels -1);
+            }
+        } else {
+            Serial.print("  FILE: ");
+            Serial.print(file.name());
+            Serial.print("  SIZE: ");
+            Serial.println(file.size());
+        }
+        file = root.openNextFile();
+    }
+}
+
+
+
+void setup(){
+    Serial.begin(115200);
+    pinMode(23,INPUT_PULLUP);
+    if(!SD.begin()){
+        Serial.println("Card Mount Failed");
+        return;
+    }
+    uint8_t cardType = SD.cardType();
+
+    if(cardType == CARD_NONE){
+        Serial.println("No SD card attached");
+        return;
+    }
+
+    Serial.print("SD Card Type: ");
+    if(cardType == CARD_MMC){
+        Serial.println("MMC");
+    } else if(cardType == CARD_SD){
+        Serial.println("SDSC");
+    } else if(cardType == CARD_SDHC){
+        Serial.println("SDHC");
+    } else {
+        Serial.println("UNKNOWN");
+    }
+    uint64_t cardSize = SD.cardSize() / (1024 * 1024);
+    Serial.printf("SD Card Size: %lluMB\n", cardSize);
+    listDir(SD, "/", 0);
+}
+void readFile(fs::FS &fs, const char * path){
+    printf("Reading file: %s\n", path);
+
+    File file = fs.open(path);
+    if(!file){
+        printf("Failed to open file for reading\n");
+        return;
+    }
+
+    printf("Read from file: ");
+    String giatritam = "";
+    giatritam = file.readStringUntil('\n');
+    printf("%s \n",giatritam.c_str());
+    file.close();
+}
+void loop(){
+ readFile(SD, "/Nhan.txt");
+ delay(10000);
+
+}
