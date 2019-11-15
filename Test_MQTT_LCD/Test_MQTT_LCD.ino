35df4adecf9128e6adc57dbb4d390b9c1cdb7f5d
Riki <annhandt09@gmail.com>
2019-11-15 14:19:10 +0700

    vietnamese eeprop

--- Test_MQTT_LCD/Test_MQTT_LCD.ino
+++ Test_MQTT_LCD/Test_MQTT_LCD.ino
@@ -18,13 +18,15 @@ extern "C" {
 #ifdef U8X8_HAVE_HW_I2C
 #include <Wire.h>
 #endif
-
+uint8_t stt_luu = 0;
 U8G2_ST7920_128X64_F_SW_SPI u8g2(U8G2_R0, /* clock=*/ 27, /* data=*/ 26, /* CS=*/ U8X8_PIN_NONE);
 static struct WiFiConfStruct {
-  char sta_ssid[100];
+  char sta_ssid[30][50];
 } WiFiConf;
-
-
+uint16_t address_
+static struct Lo_ca {
+  char sta_ssid[30][50];
+} Lo_ca;
 #define WIFI_SSID "CMA"
 #define WIFI_PASSWORD "cma2018LHP515"
 
@@ -97,16 +99,21 @@ void onMqttUnsubscribe(uint16_t packetId) {
 
 void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total) {
   Serial.print("  payload: ");
-  Serial.println(payload);
-  strncpy( WiFiConf.sta_ssid , payload, sizeof(WiFiConf.sta_ssid));
+   Serial.print(stt_luu);
+    Serial.print("  - ");
+  Serial.print(payload);
+      Serial.print("  - ");
+  Serial.println(len);
+  strcpy( WiFiConf.sta_ssid[stt_luu] , payload);
    u8g2.clearBuffer();
     u8g2.setFont(u8g2_font_unifont_t_vietnamese1);
     u8g2.setCursor(10, 12);
-     u8g2.print(payload);
-         u8g2.setCursor(10, 32);
-     u8g2.print(WiFiConf.sta_ssid);
+    u8g2.print(payload);
+    u8g2.setCursor(10, 32);
+    u8g2.print(WiFiConf.sta_ssid[stt_luu]);
     u8g2.sendBuffer();  
-savedata();
+    savedata();
+    stt_luu ++ ;
 }
 
 void onMqttPublish(uint16_t packetId) {
@@ -115,23 +122,24 @@ void onMqttPublish(uint16_t packetId) {
   Serial.println(packetId);
 }
 bool loaddata() {
-    uint8_t bien=0;
+    uint16_t bien=0;
+  // Serial.println(sizeof(WiFiConf));
     for (unsigned int t = 0; t < sizeof(WiFiConf); t++) {
       bien ++ ;
-      *((uint8_t*)&WiFiConf + t) = EEPROM.read(700 + t); //& lÃ  Ä‘á»‹a chá»‰  cá»§a biáº¿n Struc, *lÃ  data tá»©c lÃ  gÃ¡n data trong Ã´ nhá»› struc báº±ng eprom Ä‘á»�c dc (char*) lÃ  Ã©p kiá»ƒu dá»¯ liá»‡u
-      Serial.print(bien);
+      *((char*)&WiFiConf + t) = EEPROM.read(190 + t); //& lÃ  Ä‘á»‹a chá»‰  cá»§a biáº¿n Struc, *lÃ  data tá»©c lÃ  gÃ¡n data trong Ã´ nhá»› struc báº±ng eprom Ä‘á»�c dc (char*) lÃ  Ã©p kiá»ƒu dá»¯ liá»‡u
+     /* Serial.print(700 + t);
       Serial.print("-");
-      Serial.println(EEPROM.read(700 + t));
+      Serial.println(EEPROM.read(700 + t));*/
     }
     Serial.println("vvvv");
-    return true;
+  // return true;
 }
 /*
  * LÆ°u Eprom thÃ´ng tin wifi
  */
 void savedata(void) {
   for (unsigned int t = 0; t < sizeof(WiFiConf); t++) {
-    EEPROM.write(700 + t, *((uint8_t*)&WiFiConf + t));
+    EEPROM.write(190 + t, *((char*)&WiFiConf + t));
   }
   EEPROM.commit();
 }
@@ -139,20 +147,27 @@ void setup() {
   Serial.begin(115200);
   Serial.println();
   Serial.println();
-  EEPROM.begin(1024);
+   if (!EEPROM.begin(5000))
+  {
+    Serial.println("failed to initialise EEPROM"); 
+  }
   u8g2.begin();
   loaddata();
   u8g2.enableUTF8Print();
-     u8g2.clearBuffer();
-    u8g2.setFont(u8g2_font_unifont_t_vietnamese1);
-          u8g2.setCursor(0, 12);
-          u8g2.print(WiFiConf.sta_ssid);
-    u8g2.sendBuffer(); 
+  u8g2.clearBuffer();
+  u8g2.setFont(u8g2_font_unifont_t_vietnamese1);
+  u8g2.setCursor(0, 12);
+  u8g2.print(WiFiConf.sta_ssid[0]);
+  u8g2.setCursor(0, 24);
+  u8g2.print(WiFiConf.sta_ssid[1]);
+  u8g2.setCursor(0, 36);
+  u8g2.print(WiFiConf.sta_ssid[2]);
+  u8g2.setCursor(0, 48);
+  u8g2.print(WiFiConf.sta_ssid[3]);
+  u8g2.sendBuffer(); 
   mqttReconnectTimer = xTimerCreate("mqttTimer", pdMS_TO_TICKS(2000), pdFALSE, (void*)0, reinterpret_cast<TimerCallbackFunction_t>(connectToMqtt));
   wifiReconnectTimer = xTimerCreate("wifiTimer", pdMS_TO_TICKS(2000), pdFALSE, (void*)0, reinterpret_cast<TimerCallbackFunction_t>(connectToWifi));
-
   WiFi.onEvent(WiFiEvent);
-
   mqttClient.onConnect(onMqttConnect);
   mqttClient.onDisconnect(onMqttDisconnect);
   mqttClient.onSubscribe(onMqttSubscribe);
