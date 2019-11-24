426404b7440f6f3edeb94c273a124ed4cbf5aefc
Riki <annhandt09@gmail.com>
2019-11-24 18:10:13 +0700

    Modbus UTF16 ASCII and uint32_t

--- RTUSlaverUTF16/RTUSlaverUTF16.ino
+++ RTUSlaverUTF16/RTUSlaverUTF16.ino
@@ -23,31 +23,25 @@ return dest;
 #define SLAVE_ID 1
 
 ModbusRTU mb;
-char hienthiutf[100]; 
-wchar_t hienthiascii[100];
-char16_t output[100];
-
+char hienthiutf[200]; 
+wchar_t hienthiascii[150];
+//char16_t output[100];
+uint32_t sogui = 0;
 void setup() {
   Serial.begin(115200);
   Serial2.begin(9600, SERIAL_8N1);
   mb.begin(&Serial2);
   mb.slave(SLAVE_ID);
-  mb.addHreg(REGN,0x0000,100);
-  mb.addHreg(30,0x0A00,1);
+  mb.addHreg(REGN,0x0000,305);
+  mb.Hreg(300, 10);
 }
 int i = 0;
 int scenes = 10;
+long time_cho = 0;
+long delay_ms = 0;
 void loop() {
   if (Serial.available()){
     hienthiutf[i] = Serial.read();
-    /*if (tams == 'Z'){
-      //chuyển trang
-      if (scenes == 10 ){
-        mb.Hreg(300, 0x0B00);
-      }
-      else mb.Hreg(300, 0x0A00);
-    }*/
-
     if (hienthiutf[i] == '\n'){
       i = 0;
       wmemset(hienthiascii,0x0000, sizeof(hienthiascii)/2);
@@ -58,16 +52,31 @@ void loop() {
       Serial.print(sizeof(hienthiascii));
       Serial.print("  Hienthi0 ");
       Serial.println(hienthiascii[1],HEX);
-      for (int j =0 ; j < sizeof(hienthiascii)/2 ; j++){        
-        mb.Hreg(REGN + j, hienthiascii[j]);
-      }
+      for (int j =0 ; j < sizeof(hienthiascii)/2 ; j++){mb.Hreg(REGN + j, hienthiascii[j]);}
+      mb.Hreg(300, 11);
+      time_cho = millis();
+      scenes = 11;
+      mb.Hreg(300, 11);
       /*for (int j = valuelen ; j < valuelen ; j++){        
         mb.Hreg(REGN + j, hienthiascii[j]);
       }*/
       memset(hienthiutf, '\0', sizeof(hienthiutf));
     }
-    else if ( i > 99) {i=0;}
+    else if ( i > 200) {i=0;}
     else i = i+ 1;
+  }
+  if((millis() - time_cho > 5000)&&(scenes == 11)){
+    scenes = 10;
+    mb.Hreg(300, 10);
+  }
+  if(millis() - delay_ms > 1000){
+    delay_ms = millis();
+    sogui = sogui+1;
+    Serial.print(" So :");
+    Serial.print(sogui,HEX);
+    Serial.println(sogui>>16, HEX);
+    mb.Hreg(301, sogui);
+    mb.Hreg(302, sogui >> 16);
   }
   mb.task();
   yield();
